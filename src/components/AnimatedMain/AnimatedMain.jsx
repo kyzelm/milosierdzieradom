809f003d1a4b6603d78ed22341d313a441ac66cd
Damian Osiński <osinskidamian03@gmail.com>
2024-11-01 16:12:26 +0100

    HOTFIX: mobile change location error

--- src/components/AnimatedMain/AnimatedMain.jsx
+++ src/components/AnimatedMain/AnimatedMain.jsx
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
 import {motion} from "framer-motion";
+import {useLocation} from "react-router-dom";
 
 function AnimatedMain({children}) {
-    return (
-        <motion.main initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 0.5}}>
-            {children}
-        </motion.main>
-    );
+  const localtion = useLocation().pathname;
+
+  return (
+    <motion.main key={localtion} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}
+                 transition={{duration: 0.5}}>
+      {children}
+    </motion.main>
+  );
 }
 
 AnimatedMain.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default AnimatedMain;
